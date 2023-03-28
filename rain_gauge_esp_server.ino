3866ca0d25e28877754ff5836b4e3765ab3ab014
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-03-27 23:26:27 -0300

    feat: implement creating web server

--- rain_gauge_esp_server.ino
+++ rain_gauge_esp_server.ino
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
+
+constexpr auto webServerPort = 80;
+auto webServer = ESP8266WebServer(webServerPort);
 
 auto setUpSerial() -> void;
 auto setUpWiFi() -> void;
+auto setUpWebServer() -> void;
+
+auto handleRequest() -> void;
 
 auto setup() -> void
 {
 	setUpSerial();
 	setUpWiFi();
-
-	Serial.println("Hello, rain gauge ESP server world!");
+	setUpWebServer();
 }
 
-auto loop() -> void {}
+auto loop() -> void
+{
+	webServer.handleClient();
+}
 
 auto setUpSerial() -> void
 {
@@ -36,4 +45,28 @@ auto setUpWiFi() -> void
 	Serial.print("Connected to Wi-Fi network at ");
 	Serial.print(WiFi.localIP());
 	Serial.print(".\n");
+}
+
+auto setUpWebServer() -> void
+{
+	webServer.on("/", handleRequest);
+	webServer.begin();
+
+	Serial.printf("Web server started on port %d.\n", webServerPort);
+}
+
+auto handleRequest() -> void
+{
+	auto const clientIP = webServer.client().remoteIP();
+
+	Serial.print("A request was made from ");
+	Serial.print(clientIP);
+	Serial.print(".\n");
+
+	webServer.send
+	(
+		200,
+		"text/plain",
+		"Hello from the rain gauge ESP server!\n"
+	);
 }
