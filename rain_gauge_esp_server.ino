1c8d399e420e84b05da69014c5d54e9e425406ed
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-03-27 22:52:53 -0300

    feat: implement connecting to Wi-Fi network

--- rain_gauge_esp_server.ino
+++ rain_gauge_esp_server.ino
@@ -1,13 +1,39 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+auto set_up_serial() -> void;
+auto set_up_wi_fi() -> void;
 
 auto setup() -> void
+{
+	set_up_serial();
+	set_up_wi_fi();
+
+	Serial.println("Hello, rain gauge ESP server world!");
+}
+
+auto loop() -> void {}
+
+auto set_up_serial() -> void
 {
 	constexpr auto baud_rate = 9600uL;
 	Serial.begin(baud_rate);
 }
 
-auto loop() -> void
+auto set_up_wi_fi() -> void
 {
-	Serial.println("Hello, rain gauge ESP server world!");
-	delay(1000uL);
+	constexpr auto ssid = "Felipe's Galaxy S20+";
+	constexpr auto password = "esgv1755";
+
+	WiFi.begin(ssid, password);
+
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		Serial.printf("Connecting to Wi-Fi network \"%s\"...\n", ssid);
+		delay(1000uL);
+	}
+
+	Serial.print("Connected to Wi-Fi network at ");
+	Serial.print(WiFi.localIP());
+	Serial.print(".\n");
 }
